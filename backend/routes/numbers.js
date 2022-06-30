e77b23a0e36c2213de9b007778c9cffaecef3aa6
chiragthakuri <83844592+chiragthakuri@users.noreply.github.com>
2022-06-30 14:38:19 -0500

    CRUD controllers complete

--- backend/routes/numbers.js
+++ backend/routes/numbers.js
@@ -1,39 +1,28 @@
 const express = require('express')
-const Number = require('../models/numberModel')
+const {
+  createNumber, 
+  getNumbers,
+  getNumber,
+  deleteNumber,
+  updateNumber
+} = require('../controllers/numberController')
+
 
 const router = express.Router()
 
 // GET all numbers
-router.get('/', (req, res) => {
-  res.json({mssg: 'GET all numbers'})
-})
+router.get('/', getNumbers)
 
 // GET a single number
-router.get('/:id', (req, res) => {
-  res.json({mssg: 'GET a single number'})
-})
+router.get('/:id', getNumber)
 
 // POST a new number
-router.post('/', async (req, res) => {
-  const{phone_number,description,country_of_origin, displayFlag} = req.body
-
-  try {
-    const number = await Number.create({phone_number,description,country_of_origin, displayFlag})
-    res.status(200).json(number)
-  } catch (error) {
-    res.status(400).json({error: error.message})
-  }
-
-})
+router.post('/', createNumber)
 
 // DELETE a number
-router.delete('/:id', (req, res) => {
-  res.json({mssg: 'DELETE a number'})
-})
+router.delete('/:id',deleteNumber)
 
 // UPDATE a number
-router.patch('/:id', (req, res) => {
-  res.json({mssg: 'UPDATE a number'})
-})
+router.patch('/:id', updateNumber)
 
 module.exports = router
